e98e6dd42aa0ceaac6fe9881de21d0482cc32108
Zachary Atkins <atkins12@llnl.gov>
2020-06-23 15:37:12 -0700

    edit to shell script

--- makeRankExperiments.sh
+++ makeRankExperiments.sh
@@ -3,11 +3,11 @@ preparedata load mg_scale \
     replace-column --scale-col 1 --scale-by .9  4 scale \
     replace-column --weights "-0.1 -0.2 2 0 0" 3 weights \
     replace-column 2 uniform \
-    save-svm mg_scale_replace.svm
+    save-svm --overwrite mg_scale_replace.svm
 
 preparedata load mg_scale \
     insert-column --scale-col 0 --scale-by 1.1 scale \
     insert-column --scale-col 1 --scale-by 0.9 scale \
     insert-column --weights "1 2 3 4 5 6" weights \
     insert-column uniform \
-    save-svm mg_scale_insert.svm
+    save-svm --overwrite mg_scale_insert.svm
