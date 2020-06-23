34a0d9f057a10277fa7b92cc3a965a240fcfca14
Zachary Atkins <atkins12@llnl.gov>
2020-06-23 13:43:10 -0700

    added simple cli for rank experiments

--- preparedata.py
+++ preparedata.py
@@ -1,30 +1,97 @@
-from sklearn.datasets import load_svmlight_file,dump_svmlight_file
+from sklearn.datasets import load_svmlight_file, dump_svmlight_file
 from sklearn.preprocessing import normalize
 from scipy.sparse import csc_matrix
 import numpy as np
+import os
 import click
 
-def center_data(X, axis=0, copy=True):
-    m,n = X.shape
-    if not axis:
-        t = m
-        m = n
-        n = t
-    X_bar = [np.average(X[:,i].todense()) for i in range(m)]
-    if copy:
-        return np.concatenate([X[:,i].todense() - X_bar[i]*np.ones((n,)) for i in range(len(X_bar))])
-    for i in range(m):
-        X[:,i] -= X_bar[i]*np.ones((n,))
-        return X
 
-@click.command()
-@click.option('--input_file', type=click.File, help='path to svm dataset')
-@click.option('--axis', type=click.IntRange(0,1), default=0, help='if 0 [default], then centers and normalizes each col, elif 1, each row')
-@click.option('--output_file', type=click.File(mode='w'), help='output file path')
-def main(input_file, axis, overwrite):
-    X, y = load_svmlight_file(input_file)
-    X = center_data(X, axis=axis, copy=False)
-    normalize(X, axis=axis)
+class RankEditor(object):
+    def __init__(self, indir=None, outdir=False):
+        self.indir = os.path.abspath(indir or '../data')
+        self.outdir = os.path.abspath(outdir or 'data')
+        self.data = None
+        self.y = None
+        
 
-if __name__ == '__main__':
-    main()
+pass_editor = click.make_pass_decorator(RankEditor)
+
+@click.group(chain=True)
+@click.option('--indir', default=os.path.join('..', 'data'))
+@click.option('--outdir', default='data')
+@click.pass_context
+def cli(ctx, indir, outdir):
+    ctx.obj = RankEditor(indir, outdir) 
+
+@cli.command('load')
+@click.argument('dataset')
+@click.option('--confirm', is_flag=True)
+@pass_editor
+def load(editor, dataset, confirm):
+    if editor.data is not None and not confirm:
+        print("There is existing data, pass --confirm flag to load anyway")
+        return False
+    
+    if '.svm' not in dataset:
+        dataset += '.svm'
+    path = os.path.join(editor.indir, dataset)
+    assert os.path.exists(path), f"SVM file '{path}' not found"
+    old_data = editor.data.copy() if editor.data is not None else None
+    old_y = editor.y.copy() if editor.y is not None else None
+    try:
+        editor.data, editor.y = load_svmlight_file(path)
+        editor.data = editor.data.tocsc()
+        print(f"Loaded '{dataset}', shape {editor.data.shape}")
+        return
+    except Exception as e:
+        print(e)
+        editor.data = old_data
+        editor.y = old_y
+        return
+
+@cli.command('replace-column')
+@click.argument('col', type=click.INT)
+@click.option('--uniform', is_flag=True, help="if true, all remaining columns are given equal weighting of 1/(n_col - 1)")
+@click.option('--weights', type=click.STRING, default=None,
+    help="string containing python array with length n_col-1."
+         "values in array correspond weights of each remaining column for replacement linear combination."
+         "overwritten by `--uniform` flag")
+@pass_editor
+def replace_column(editor, col, uniform, weights):
+    print('entered replace')
+    assert editor.data is not None, "load data before attempting to edit"
+    assert weights is not None or uniform, "either specify weights or use the `--uniform` flag"
+    
+    n_row,n_col = editor.data.shape
+    if uniform:
+        weights = np.array([1/(n_col-1)]*(n_col-1))
+    else:
+        weights = np.loads(weights)
+
+    weights = np.insert(weights, col, 0)
+
+    new_col = editor.data * weights
+    from scipy.sparse import csc_matrix
+    editor.data[:,col] = csc_matrix(new_col.reshape((n_row,1)))
+    return
+
+@cli.command('save-svm')
+@click.argument('filename', type=click.STRING)
+@click.option('--overwrite', is_flag=True)
+@pass_editor
+def save_svm(editor, filename, overwrite):
+    print('entered save')
+    assert editor.data is not None, "no data is loaded"
+    path = os.path.join(editor.indir, filename)
+    if os.path.exists(path) and not overwrite:
+        print(f"Error: '{path}' already exists, use `--overwrite` to save anyway")
+        return
+    elif os.path.exists(path):
+        print(f"Warning: '{path}' already exists, overwriting")
+        os.remove(path)
+
+    dump_svmlight_file(editor.data, editor.y, path)
+    return
+
+if __name__ == "__main__":
+    cli()
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 import numpy
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup, Extension
+import setuptools
 from Cython.Build import cythonize
 # from sklearn._build_utils import get_blas_info
 
@@ -13,18 +13,19 @@ from Cython.Build import cythonize
 include_dirs = [numpy.get_include()]# + blas_info.pop('include_dirs', [])
 
 ext_modules = [
-    Extension("fast_cd.svm", sources=["fast_cd/svm.pyx"], libraries=["m"], include_dirs=include_dirs),
-    Extension("fast_cd.elasticnet", sources=["fast_cd/elasticnet.pyx"], libraries=["m"], include_dirs=include_dirs)
-]
-
-ext_modules = [
     Extension("fast_cd.svm", sources=["fast_cd/svm.pyx"], include_dirs=include_dirs),
     Extension("fast_cd.elasticnet", sources=["fast_cd/elasticnet.pyx"], include_dirs=include_dirs)
 ]
 
-setup(name="CoLA",
-      ext_modules=cythonize(ext_modules, compiler_directives={'language_level': 3}),
-      # include_dirs=include_dirs,
-      packages=[
-          'cola',
-          'fast_cd'])
+setup(
+    name="CoLA",
+    ext_modules=cythonize(ext_modules, compiler_directives={'language_level': 3}),
+    # include_dirs=include_dirs,
+    packages=[
+        'cola',
+        'fast_cd'],
+    entry_points='''
+        [console_scripts]
+        preparedata=preparedata:cli
+        run_cola=run_cola:main
+    ''')
