d1e633de3f903f6d976d76995cac94a8fd66cbb0
Zachary Atkins <atkins12@llnl.gov>
2020-06-23 17:25:14 -0700

    add insertion to experiment script

--- runRankExperiments.sh
+++ runRankExperiments.sh
@@ -1,6 +1,5 @@
 replace_dataset="mg_scale_replace"
-insert_dataset="mg_scale_replace"
-
+insert_dataset="mg_scale_insert"
 
 colatools load mg_scale \
     replace-column --scale-col 0 --scale-by 1.1 5 scale \
@@ -34,13 +33,18 @@ global_alg='cola'
 topology='complete'
 
 echo "Rank: Column Replacement"
+
 dataset=$replace_dataset
+
+# Clear old logs and plots
+rm -rf './log/'$dataset'/*' './out/'$dataset'/*'
+
 for world_size in {1..6};
 do
     log_path=${OUTPUT_DIR}'/'$dataset'/'$world_size'/'
     # Run cola
     echo "|-> Running CoLA, world size="$world_size
-    mpirun -n $world_size -output-filename $logpath'mpilog' --oversubscribe python3 run_cola.py \
+    mpirun -n $world_size --output-filename $logpath'mpilog' --oversubscribe python3 run_cola.py \
         --split_by 'features' \
         --max_global_steps $global_steps \
         --graph_topology $topology \
@@ -64,3 +68,42 @@ done;
 
 # Clean up
 rm -rf ./data/$dataset
+echo "END: Column Replacement"
+
+echo "Rank: Column Insertion"
+
+dataset=$insert_dataset
+
+# Clear old logs and plots
+rm -rf './log/'$dataset'/*' './out/'$dataset'/*'
+
+for world_size in {1..10};
+do
+    log_path=${OUTPUT_DIR}'/'$dataset'/'$world_size'/'
+    # Run cola
+    echo "|-> Running CoLA, world size="$world_size
+    mpirun -n $world_size --output-filename $logpath'mpilog' --oversubscribe python3 run_cola.py \
+        --split_by 'features' \
+        --max_global_steps $global_steps \
+        --graph_topology $topology \
+        --exit_time 1000.0 \
+        --logmode='all' \
+        --theta $theta \
+        --l1_ratio $l1_ratio \
+        --lambda_ $lambda \
+        --output_dir ${OUTPUT_DIR} \
+        --dataset_size 'all' \
+        --ckpt_freq 1 \
+        --dataset $dataset \
+        --solvername $local_alg \
+        --algoritmname $global_alg \
+        --use_split_dataset \
+    	&> /dev/null;
+    # Save result plot
+    echo "|-> Saving result plots to 'out/"$dataset"/"$world_size"/'.."
+    viewresults --dataset $dataset --k $world_size --no-show --save &> /dev/null;
+done;
+
+# Clean up
+rm -rf ./data/$dataset
+echo "END: Column Insertion"
